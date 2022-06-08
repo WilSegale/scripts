c39c8b603d72a9ac18a1b9b942e17a96fae36a82
anona <39059459+WilSegale@users.noreply.github.com>
2022-06-07 20:59:44 -0400

    game

--- mac/hydar.sh
+++ mac/hydar.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo rm -rf output.txt
 clear
-echo "Loading..."
+echo "Loading..." && say "Loading"
 for ((k = 0; k <= 10; k++)); do
     echo -n "[ "
-    for ((i = 0; i <= k; i++)); do echo -n "-"; done
+    for ((i = 0; i <= k; i++)); do echo -n "|||"; done
     sleep 1
     for ((j = i; j <= 10; j++)); do echo -n "  "; done
     v=$((k * 10))
@@ -15,19 +15,23 @@ echo
 chmod +x *
 
 clear
-echo 'Starting...'
+echo 'Starting...' && say "Starting..."
 function run() {
     clear
     read -p "Do you want SSH(22) or VNC(5900). To see all type (ALL): " service
 
     if [[ $service == "ALL" ]]; then
         sudo nmap -sS 192.168.1.1/24 --open
+        say "Scan is done"
+
 
     elif [[ $service == "all" ]]; then
         sudo nmap -sS 192.168.1.1/24 --open
-
+        say "Scan is done"
     else
         sudo nmap -sS 192.168.1.1/24 -p $service --open
+        say "Scan is done"
+
     fi
 
     echo "To crack VNC(5900) type 'NONE' in the input username"
