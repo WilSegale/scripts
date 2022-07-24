b21e7869dc1a718ba94ad9316224d987a4661dcb
anona <39059459+WilSegale@users.noreply.github.com>
2022-07-24 17:28:53 -0400

    script

--- mac/hydra.sh
+++ mac/hydra.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 chmod +x *
+wget -q --spider http://google.com
 
+if [ $? -eq 0 ]; then
+    echo ""
+else
+    echo "You are Offline please connect to the internet"
+    exit
+fi
 clear
 echo "Loading..."
 for ((k = 0; k <= 10; k++)); do
@@ -13,14 +20,7 @@ for ((k = 0; k <= 10; k++)); do
             echo -n "$v%" $'\r'
 done
 echo
-wget -q --spider http://google.com
 
-if [ $? -eq 0 ]; then
-    echo ""
-else
-    echo "You are Offline please connect to the internet"
-    exit
-fi
 clear
 echo 'Starting...'
 function run() {
